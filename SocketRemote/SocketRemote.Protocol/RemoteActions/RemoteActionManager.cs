d7be29b18fd4fd0ab789d73d057f771089539d0b
SamLangTen <samlangten@outlook.com>
2018-12-05 16:11:53 +0800

    add remote loader

--- SocketRemote/SocketRemote.Protocol/RemoteActions/RemoteActionManager.cs
+++ SocketRemote/SocketRemote.Protocol/RemoteActions/RemoteActionManager.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Reflection;
+using System.Linq;
 namespace SocketRemote.Protocol.RemoteActions
 {
-    class RemoteActionManager
+    public class RemoteActionManager
     {
+        public IList<Type> GetAllRemoteActions()
+        {
+            var types = new List<Type>();
+            AppDomain.CurrentDomain.GetAssemblies().Select(a => a.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(IRemoteAction)))).ToList().ForEach(t=>types.AddRange(t));
+            return types;
+        }
+
+        public IList<IRemoteAction> GetAllRemoteActionInstances()
+        {
+            return GetAllRemoteActions().Select(t => (IRemoteAction)Activator.CreateInstance(t)).ToList();
+        }
     }
 }
