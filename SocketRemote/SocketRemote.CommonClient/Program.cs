5f3e1f840c90834f43f2b13c92226432ba069090
SamLangTen <samlangten@outlook.com>
2018-12-23 16:45:52 +0800

    Add sra and common client

--- SocketRemote/SocketRemote.CommonClient/Program.cs
+++ SocketRemote/SocketRemote.CommonClient/Program.cs
@@ -8,7 +8,7 @@ using SocketRemote.Protocol.RemoteActions;
 using System.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace SocketRemote.CommonClient
 {
     class Program
@@ -21,16 +21,32 @@ namespace SocketRemote.CommonClient
             var host = Console.ReadLine();
             Console.Write("Port:");
             var port = Console.ReadLine();
-            while(true)
+            while (true)
             {
-                var client = new SocketRemoteClient("127.0.0.1", 8540, Encoding.UTF8.GetBytes(keyString));
-                Console.Write("Filename:");
-                var filename = Console.ReadLine();
-                var command = new FileSystemRemoteAction();
-                command.CommandProperties.Add("Action", "ls");
-                command.CommandProperties.Add("Filename", filename);
+                var client = new SocketRemoteClient(host, int.Parse(port), Encoding.UTF8.GetBytes(keyString));
+                //加载可用的RemoteAction
+                var actionsType = RemoteActionManager.GetAllRemoteActions();
+                var actionsInstance = RemoteActionManager.GetAllRemoteActionInstances();
+                Console.WriteLine("Available RA:");
+                Console.WriteLine(string.Join("\n", actionsInstance.Select(a => a.ActionId.ToString() + ":" + a.GetType().Name)));
+                Console.Write("Select:");
+                var selected = int.Parse(Console.ReadLine());
+                //初始化RA
+                var ra = actionsInstance.FirstOrDefault(t => t.ActionId == selected);
+                Console.WriteLine("\"Property=Value\" To set action properties");
+                Console.WriteLine("\"Send\" To send action");
+                var command = Console.ReadLine();
+                while (command.ToLower().Trim() != "send")
+                {
+                    if (!command.Contains("="))
+                        continue;
+                    var prop = command.Split('=')[0];
+                    var value = string.Join("=", command.Split('=').Skip(1).ToArray());
+                    ra.CommandProperties.Add(prop, value);
+                    command = Console.ReadLine();
+                }
                 client.Connect();
-                var res = client.SendCommand(new IRemoteAction[] { command }, new TimeSpan(0, 0, 30)).GetEnumerator();
+                var res = client.SendCommand(new IRemoteAction[] { ra }, new TimeSpan(0, 0, 30)).GetEnumerator();
                 while (res.MoveNext())
                 {
                     var a = res.Current;
---
+++ SocketRemote/SocketRemote.Protocol/RemoteActions/Actions/ShellRemoteAction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Linq;
+using SocketRemote.Protocol.RemoteActions;
+using System.IO;
+
+namespace SocketRemote.Protocol.RemoteActions.Actions
+{
+    public class ShellRemoteAction : IRemoteAction
+    {
+        public int ActionId => 2;
+
+        public Dictionary<string, string> CommandProperties { get; set; } = new Dictionary<string, string>();
+
+        public ActionExecutionResult Execute(byte[] command)
+        {
+            try
+            {
+                var commandString = new string(Encoding.UTF8.GetChars(command));
+                var cmd = commandString.Split('\n')[0];
+                var arg = commandString.Split('\n')[1];
+                var process = new Process();
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.FileName = cmd;
+                process.StartInfo.Arguments = arg;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+                process.WaitForExit(2000);
+                process.Kill();
+                var output = process.StandardOutput.ReadToEnd();
+                return new ActionExecutionResult()
+                {
+                    Message = Encoding.UTF8.GetBytes(output),
+                    State = ActionExecutionState.Success
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ActionExecutionResult()
+                {
+                    Message = Encoding.UTF8.GetBytes(ex.Message),
+                    State = ActionExecutionState.Failed
+                };
+            }
+        }
+
+        public string GetServerCommand()
+        {
+            var command = CommandProperties.FirstOrDefault(kvp => kvp.Key == "Filename");
+            var arg = CommandProperties.FirstOrDefault(kvp => kvp.Key == "Arguments");
+            return command.Value + "\n" + arg.Value;
+        }
+    }
+}
